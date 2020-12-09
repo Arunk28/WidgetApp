25ede3ea7c2444ac426734ba5ee175d395daad4c
Arunk28 <arunkumar280491@gmail.com>
2020-12-09 22:09:53 +0530

    search component wikipedia

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,7 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState(null);
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+  useEffect = () => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          orgin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data);
+    };
+  };
+  const renderResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="ui segment">
       <form className="ui form" onSubmit={this.onSearch}>
