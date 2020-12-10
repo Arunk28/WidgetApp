4ebb69a4a43f750fef605e58e64979345683a98a
Arunk28 <arunkumar280491@gmail.com>
2020-12-10 22:13:09 +0530

    wikipedia component ready

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -4,20 +4,22 @@ import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("programming");
   const [results, setResults] = useState([]);
-  useEffect = () => {
+
+  useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
-          orgin: "*",
+          origin: "*",
           format: "json",
           srsearch: term,
         },
       });
-      setResults(data);
+      setResults(data.query.search);
     };
-  };
+  }, [term]);
+
   const renderResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
@@ -31,18 +33,19 @@ const Search = () => {
 
   return (
     <div className="ui segment">
-      <form className="ui form" onSubmit={this.onSearch}>
+      <form className="ui form">
         <div className="field">
           <label> Search</label>
           <input
             type="text"
-            value={this.state.term}
+            value={term}
             onChange={(e) => {
               setTerm(e.target.value);
             }}
           ></input>
         </div>
       </form>
+      <div className="ui celled list">{renderResults}</div>
     </div>
   );
 };
